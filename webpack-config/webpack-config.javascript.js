118c35cf704a377b01b26e05faa3d892f3a0ceb9
SeanJM <seanjmacisaac@gmail.com>
2018-05-19 22:49:20 -0400

    Updated webpack config to build React

--- webpack-config/webpack-config.javascript.js
+++ webpack-config/webpack-config.javascript.js
@@ -1,48 +1,57 @@
-const { HotModuleReplacementPlugin } = require("webpack");
+const { HotModuleReplacementPlugin, DefinePlugin } = require("webpack");
 const path = require("path");
 const __root = path.resolve(__dirname, "..");
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "development",
   devtool: process.env.NODE_ENV === "production" ? false : "source-map",
+
   devServer: {
     contentBase: path.join(__root, "dist/"),
     hot: true, // This is what allows to just replace a module (component)
     port: 3000,
-    open: true
+    open: true,
   },
-  entry: "./src/index.js",
+
+  entry: "./src/index.jsx",
   output: {
     path: path.resolve(__root, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
+
   resolve: {
-    extensions: [".js"],
+    extensions: [".js", ".jsx"],
     alias: {
-      "@h": path.resolve(__root, "src/scripts/hyperscript.js"),
-      "@components": path.resolve(__root, "src/components/"),
-      "@class": path.resolve(__root, "src/class/"),
-      "@store": path.resolve(__root, "src/store"),
-      "@scripts": path.resolve(__root, "src/scripts"),
-      "@app": path.resolve(__root, "src/app"),
       "@deep": path.resolve(__root, "src/scripts/deep"),
       "@": path.resolve(__root, "src/"),
-    }
+      "@components": path.resolve(__root, "src/components"),
+      "@store": path.resolve(__root, "src/store"),
+      "@action": path.resolve(__root, "src/action"),
+    },
   },
+
   module: {
     rules: [{
-      test: /\.js$/,
+      test: /\.js(x|)$/,
       exclude: /node_modules/,
       use: {
         loader: "babel-loader",
         options: {
           presets: ["env"],
-          plugins: ["transform-object-rest-spread"]
-        }
-      }
-    }]
+          plugins: [
+            "transform-object-rest-spread",
+            // JSX syntax with Preact
+            ["transform-react-jsx"],
+          ],
+        },
+      },
+    }],
   },
+
   plugins: [
     new HotModuleReplacementPlugin(),
-  ]
-}
+    new DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    }),
+  ],
+};
