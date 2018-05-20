84b708a53491b89ebefd1c6397531cdf2e7a4d81
SeanJM <seanjmacisaac@gmail.com>
2018-05-19 22:49:47 -0400

    added auto prefixer

--- webpack-config/webpack-config.styles.js
+++ webpack-config/webpack-config.styles.js
@@ -1,25 +1,46 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const __root = path.resolve(__dirname, "..");
 
 module.exports = {
   resolve: {
     extensions: [".scss"],
+    alias: {
+      "@styles": path.resolve(__root, "src/styles"),
+    },
   },
+
   module: {
     rules: [{
       test: /\.scss$/,
       use: [
         MiniCssExtractPlugin.loader,
-        "css-loader", // translates CSS into CommonJS and adds auto prefixing
-        "sass-loader" // compiles Sass to CSS
-      ]
-    }]
+        "css-loader", // translates CSS into CommonJS
+        "sass-loader", // compiles Sass to CSSß
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: process.env.NODE_ENV === "development"
+              ? [
+                require("autoprefixer")(), // Adds browser prefixes (-moz-, -o-, '-ms-, etc...)
+              ]
+              : [
+                require("autoprefixer")(),
+                require("cssnano")(), // Compresses CSS
+              ],
+          },
+        },
+      ],
+    }],
   },
+
   plugins: [
     new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-}
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
