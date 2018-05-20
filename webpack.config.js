ac1c8eac545b8737ff6dbeefd59ebc3038478253
SeanJM <seanjmacisaac@gmail.com>
2018-05-19 22:49:40 -0400

    reordered imports

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const fs = require("fs");
-const __root = path.resolve(__dirname, "webpack-config");
 const merge = require("webpack-merge");
+const __root = path.resolve(__dirname, "webpack-config");
 
 module.exports = [
   merge(
     // We merge our multiple configuration files into one
     fs
       .readdirSync(__root)
-      .filter(filename => /^webpack-config/.test(filename) && !/test.js$/.test(filename))
-      .map(filename => require(path.join(__root, filename)))
-  )
+      .filter((filename) => (
+        /^webpack-config/.test(filename) && !/test.js$/.test(filename))
+      )
+      .map((filename) => (
+        require(path.join(__root, filename)))
+      )
+  ),
 ].concat(
   // Include our tests
   require("./webpack-config/webpack-config.test.js")
